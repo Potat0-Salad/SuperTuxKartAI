159c677bfa422551506b75958397eea2f16a84f5
Potat0-Salad <90644221+Potat0-Salad@users.noreply.github.com>
2024-06-12 17:46:03 +0200

    fixed the include paths for torch.h

--- stk-code/src/karts/controller/soccer_ai.cpp
+++ stk-code/src/karts/controller/soccer_ai.cpp
@@ -424,38 +424,38 @@ void SoccerAI::findTarget()
         m_target_point = determineBallAimingPosition();
         m_target_node  = m_world->getBallNode();
         
-    // findClosestKart(true/*consider_difficulty*/, false/*find_sta*/);
-    // // Check if this AI kart is the one who will chase the ball
-    // if (m_world->getBallChaser(m_cur_team) == (signed)m_kart->getWorldKartId())
-    // {
-    //     m_target_point = determineBallAimingPosition();
-    //     m_target_node  = m_world->getBallNode();
-    //     return;
-    // }
+    findClosestKart(true/*consider_difficulty*/, false/*find_sta*/);
+    // Check if this AI kart is the one who will chase the ball
+    if (m_world->getBallChaser(m_cur_team) == (signed)m_kart->getWorldKartId())
+    {
+        m_target_point = determineBallAimingPosition();
+        m_target_node  = m_world->getBallNode();
+        return;
+    }
 
-    // // Always reset this flag,
-    // // in case the ball chaser lost the ball somehow
-    // m_overtake_ball = false;
+    // Always reset this flag,
+    // in case the ball chaser lost the ball somehow
+    m_overtake_ball = false;
 
-    // if (m_kart->getPowerup()->getType() == PowerupManager::POWERUP_NOTHING &&
-    //     m_kart->getAttachment()->getType() != Attachment::ATTACH_SWATTER)
-    // {
-    //     tryCollectItem(&m_target_point , &m_target_node);
-    // }
-    // else if (m_world->getAttacker(m_cur_team) == (signed)m_kart
-    //     ->getWorldKartId())
-    // {
-    //     // This AI will attack the other team ball chaser
-    //     int id = m_world->getBallChaser(m_opp_team);
-    //     const AbstractKart* kart = m_world->getKart(id);
-    //     m_target_point = kart->getXYZ();
-    //     m_target_node  = m_world->getSectorForKart(kart);
-    // }
-    // else
-    // {
-    //     m_target_point = m_closest_kart_point;
-    //     m_target_node  = m_closest_kart_node;
-    // }
+    if (m_kart->getPowerup()->getType() == PowerupManager::POWERUP_NOTHING &&
+        m_kart->getAttachment()->getType() != Attachment::ATTACH_SWATTER)
+    {
+        tryCollectItem(&m_target_point , &m_target_node);
+    }
+    else if (m_world->getAttacker(m_cur_team) == (signed)m_kart
+        ->getWorldKartId())
+    {
+        // This AI will attack the other team ball chaser
+        int id = m_world->getBallChaser(m_opp_team);
+        const AbstractKart* kart = m_world->getKart(id);
+        m_target_point = kart->getXYZ();
+        m_target_node  = m_world->getSectorForKart(kart);
+    }
+    else
+    {
+        m_target_point = m_closest_kart_point;
+        m_target_node  = m_closest_kart_node;
+    }
 
 }   // findTarget
 
